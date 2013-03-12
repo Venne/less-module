62fac05fb555f48fe2944d9ed9c7de372022102d
pave <pave.pr@gmail.com>
2013-03-12 09:25:17 +0100

    Static -> dynamic methods.

--- LessModule/Macros/LessMacro.php
+++ LessModule/Macros/LessMacro.php
@@ -20,40 +20,58 @@ class LessMacro extends \Nette\Latte\Macros\MacroSet
 {
 
 	/** @var Venne\Module\Helpers */
-	protected static $moduleHelpers;
-	
-	/** @var string */
-	protected static $wwwCacheDir;
-	
-	/** @var string */
-	protected static $wwwDir;
-	
-	/** @var string */
-	protected static $debugMode = FALSE;
+	protected $moduleHelpers;
 
-	
+	/** @var string */
+	protected $wwwCacheDir;
+
+	/** @var string */
+	protected $wwwDir;
+
+	/** @var string */
+	protected $debugMode = FALSE;
+
+
 	public function setModuleHelpers($moduleHelpers)
 	{
 		$this->moduleHelpers = $moduleHelpers;
 	}
-		
 
-	public static function filter(\Nette\Latte\MacroNode $node, $writer)
+
+	public function setWwwCacheDir($wwwCacheDir)
+	{
+		$this->wwwCacheDir = $wwwCacheDir;
+	}
+
+
+	public function setWwwDir($wwwDir)
+	{
+		$this->wwwDir = $wwwDir;
+	}
+
+
+	public function setDebugMode($debugMode)
+	{
+		$this->debugMode = $debugMode;
+	}
+
+
+	public function filter(\Nette\Latte\MacroNode $node, $writer)
 	{
 		$path = $node->tokenizer->fetchWord();
 		$params = $writer->formatArray();
-		$path = self::$moduleHelpers->expandPath($path, 'Resources/public');
+		$path = $this->moduleHelpers->expandPath($path, 'Resources/public');
 
-		if(self::$debugMode) {
+		if (!$this->debugMode) {
 			$less = new \lessc();
 
 			$file = new \SplFileInfo($path);
 			$targetFile = $file->getBasename() . '-' . md5($path . filemtime($path)) . '.css';
-			$targetDir = self::$wwwCacheDir . '/less';
-			$target = $targetDir  . '/' . $targetFile;
-			$targetUrl = substr($target, strlen(self::$wwwDir));
+			$targetDir = $this->wwwCacheDir . '/less';
+			$target = $targetDir . '/' . $targetFile;
+			$targetUrl = substr($target, strlen($this->wwwDir));
 
-			if(!file_exists($targetDir)) {
+			if (!file_exists($targetDir)) {
 				umask(0000);
 				mkdir($targetDir, 0777, true);
 			}
@@ -63,11 +81,11 @@ class LessMacro extends \Nette\Latte\Macros\MacroSet
 			return ('$control->getPresenter()->getContext()->getService("assets.assetManager")->addStylesheet("' . $targetUrl . '", ' . $params . '); ');
 		} else {
 			return ('
-				$_less_file = new \SplFileInfo("'.$path.'");
-				$_less_targetFile = $_less_file->getBasename() .  \'-\' . md5(\''.$path.'\') . \'-\' . md5(\''.$path.'\' . filemtime("'.$path.'")) . \'.css\';
-				$_less_targetDir = \''.self::$wwwCacheDir.'/less\';
+				$_less_file = new \SplFileInfo("' . $path . '");
+				$_less_targetFile = $_less_file->getBasename() .  \'-\' . md5(\'' . $path . '\') . \'-\' . md5(\'' . $path . '\' . filemtime("' . $path . '")) . \'.css\';
+				$_less_targetDir = \'' . $this->wwwCacheDir . '/less\';
 				$_less_target = $_less_targetDir  . \'/\' . $_less_targetFile;
-				$_less_targetUrl = substr($_less_target, strlen(\''.self::$wwwDir.'\'));
+				$_less_targetUrl = substr($_less_target, strlen(\'' . $this->wwwDir . '\'));
 
 				if (!file_exists($_less_target)) {
 					$_less = new \lessc();
@@ -77,11 +95,11 @@ class LessMacro extends \Nette\Latte\Macros\MacroSet
 					}
 
 					// Remove old files
-					foreach (\Nette\Utils\Finder::findFiles($_less_file->getBasename() . \'-\' . md5(\''.$path.'\') . \'-*\')->from($_less_targetDir) as $_less_old) {
+					foreach (\Nette\Utils\Finder::findFiles($_less_file->getBasename() . \'-\' . md5(\'' . $path . '\') . \'-*\')->from($_less_targetDir) as $_less_old) {
 						unlink($_less_old->getPathname());
 					}
 
-					$_less->compileFile(\''.$path.'\', $_less_target);
+					$_less->compileFile(\'' . $path . '\', $_less_target);
 				}
 
 				$control->getPresenter()->getContext()->getService("assets.assetManager")->addStylesheet($_less_targetUrl, ' . $params . ');
@@ -92,12 +110,13 @@ class LessMacro extends \Nette\Latte\Macros\MacroSet
 
 	public static function install(\Nette\Latte\Compiler $compiler, Venne\Module\Helpers $moduleHelpers = NULL, $wwwCacheDir = NULL, $wwwDir = NULL, $debugMode = NULL)
 	{
-		self::$moduleHelpers = $moduleHelpers;
-		self::$wwwCacheDir = $wwwCacheDir;
-		self::$wwwDir = $wwwDir;
-		self::$debugMode = (bool)$debugMode;
-		
-		$me = new static($compiler);
+		$me = new LessMacro($compiler);
+
+		$me->moduleHelpers = $moduleHelpers;
+		$me->wwwCacheDir = $wwwCacheDir;
+		$me->wwwDir = $wwwDir;
+		$me->debugMode = (bool) $debugMode;
+
 		$me->addMacro('less', array($me, "filter"));
 	}
 
